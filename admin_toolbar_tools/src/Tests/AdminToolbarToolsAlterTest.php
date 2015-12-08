bee65c1cfadd35399d4999e0eb49854494112128
eme <wilfrid.roze@gmail.com>
2015-12-08 20:03:51 +0100

    Refactor basic tests.

--- admin_toolbar_tools/src/Tests/AdminToolbarToolsAlterTest.php
+++ admin_toolbar_tools/src/Tests/AdminToolbarToolsAlterTest.php
@@ -5,16 +5,16 @@
  * Contains \Drupal\admin_toolbar\Tests\AdminToolbarAlterTest.
  */
 
-namespace Drupal\admin_toolbar\Tests;
+namespace Drupal\admin_toolbar_tools\Tests;
 
 use Drupal\simpletest\WebTestBase;
 
 /**
- * Test the existence of Admin Toolbar module.
+ * Test the existence of Admin Toolbar tools new links.
  *
- * @group admin_toolbar
+ * @group admin_toolbar_tools
  */
-class AdminToolbarAlterTest extends WebTestBase {
+class AdminToolbarToolsAlterTest extends WebTestBase {
 
   /**
    * A user with permission to access the administrative toolbar.
@@ -34,18 +34,17 @@ class AdminToolbarAlterTest extends WebTestBase {
     parent::setUp();
 
     // Create an administrative user and log it in.
-    $this->adminUser = $this->drupalCreateUser(array('access toolbar', 'administer modules', 'access administration pages'));
+    $this->adminUser = $this->drupalCreateUser(array('access toolbar', 'access administration pages'));
     $this->drupalLogin($this->adminUser);
   }
 
   /**
    * Tests for a the hover of sub menus.
    */
-  function testAdminToolbar() {
+  function testAdminToolbarTools() {
 
-    // Assert that expanded links are present in HTML.
-    // Test with the uninstall link that must be there whatever modules exists.
-    $this->assertRaw('id="toolbar-link-system-modules_uninstall"');
+    // Test for admin_toolbar_tools if special menu items are added.
+    $this->assertRaw('id="toolbar-link-admin_toolbar_tools-flush"');
 
   }
 
